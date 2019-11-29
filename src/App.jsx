c51ccf5573b17fdeafa84f468669805274bc7ab6
Andreas <andreasjj@gmail.com>
2019-11-29 16:51:04 +0100

    Changed App from component to hook (#75)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { compose } from 'recompose';
-import { withFirebase } from './sharedComponents/Firebase';
+import { FirebaseContext } from './sharedComponents/Firebase';
 
 import { history } from './state/helpers/history';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { alertActions } from './state/actions/alert';
 import { userActions } from './state/actions/user';
 
@@ -36,94 +36,77 @@ import OpenRoute from './sharedComponents/OpenRoute';
 
 import GlobalStyle from './global-styles';
 
-class App extends React.Component {
-	constructor(props) {
-        super(props);
+const App = (props) => {
+    const firebase = useContext(FirebaseContext)
+    const dispatch = useDispatch()
 
-        this.state = {
-	      isLoading: true
-	    };
-	    
-        history.listen(function(location, action) {
+    const alert = useSelector(state => state.alert)
+
+    const [isLoading, setIsLoading] = useState(true)
+    let listener = null
+
+    useEffect(() => {
+        history.listen((location, action) => {
             // clear alert on location change
-            this.setState({isLoading: true})
-            this.removeAlert();
-        }.bind(this));
+            setIsLoading(true)
+            removeAlert();
+        });
 
-        this.finishLoading = this.finishLoading.bind(this)
-        this.removeAlert = this.removeAlert.bind(this)
+        listener = firebase.onAuthUserListener(
+          authUser => {
+            dispatch(userActions.setUser(authUser));
+          },
+          () => {
+            dispatch(userActions.setUser(null));
+          },
+        );
+
+        return () => {
+            listener()
+        }
+    }, [])
+
+    const finishLoading = () => {
+        setIsLoading(false)
     }
 
-    componentDidMount() {
-	   	const {dispatch} = this.props
-
-	    this.listener = this.props.firebase.onAuthUserListener(
-	      authUser => {
-	        dispatch(userActions.setUser(authUser));
-	      },
-	      () => {
-	        dispatch(userActions.setUser(null));
-	      },
-	    );
-    }
-
-    componentWillUnmount() {
-      this.listener();
-  	}
-
-    finishLoading() {
-    	this.setState({isLoading: false})
-    }
-
-    removeAlert() {
-    	const { dispatch } = this.props;
+    const removeAlert = () => {
     	dispatch(alertActions.clear());
     }
 
-    render() {
-    	return (
-			<div>
-				{this.props.alert.type ? <Alert alert={this.props.alert} removeToast={this.removeAlert} /> : null}
-				<Router history={history}>
-					<Route
-			          render={({ location }) => (
-			            <React.Fragment>
-			            	<Loader isLoading={this.state.isLoading} location={location} />
-			            	<Switch>
-						      <OpenRoute exact path='/' component={Splash} finishLoading={this.finishLoading} />
-						      <OpenRoute exact path='/login' component={Login} finishLoading={this.finishLoading} />
-						      <OpenRoute exact path='/register' component={Register} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/logout' component={Logout} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/dashboard' component={Dashboard} content={Home} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/overview' component={Dashboard} content={Overview} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/backlog' component={Dashboard} content={Backlog} finishLoading={this.finishLoading} />
-						      <PrivateRoute path='/backlog/issue/:id' component={Dashboard} content={IssuePage} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/labels' component={Dashboard} content={Labels} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/sprints' component={Dashboard} content={Sprints} finishLoading={this.finishLoading} />
-						      <PrivateRoute path='/sprints/:id' component={Dashboard} content={SprintPage} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/sprintboard' component={Dashboard} content={Sprintboard} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/planning' component={Dashboard} content={Planning} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/planning/game/:id' component={Dashboard} content={PlanningPokerGame} finishLoading={this.finishLoading} />
-						      <PrivateRoute exact path='/retrospective' component={Dashboard} content={Retrospective} finishLoading={this.finishLoading} />
-						      <Route render={(props) => (<Error404 finishLoading={this.finishLoading} {...props} />)} />
-						    </Switch>
-			            </React.Fragment>
-			          )} />
-				    
-			    </Router>
-			    <GlobalStyle />
-		    </div>
+	return (
+		<div>
+			{alert.type ? <Alert alert={alert} removeToast={removeAlert} /> : null}
+			<Router history={history}>
+				<Route
+		          render={({ location }) => (
+		            <React.Fragment>
+		            	<Loader isLoading={isLoading} location={location} />
+		            	<Switch>
+					      <OpenRoute exact path='/' component={Splash} finishLoading={finishLoading} />
+					      <OpenRoute exact path='/login' component={Login} finishLoading={finishLoading} />
+					      <OpenRoute exact path='/register' component={Register} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/logout' component={Logout} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/dashboard' component={Dashboard} content={Home} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/overview' component={Dashboard} content={Overview} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/backlog' component={Dashboard} content={Backlog} finishLoading={finishLoading} />
+					      <PrivateRoute path='/backlog/issue/:id' component={Dashboard} content={IssuePage} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/labels' component={Dashboard} content={Labels} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/sprints' component={Dashboard} content={Sprints} finishLoading={finishLoading} />
+					      <PrivateRoute path='/sprints/:id' component={Dashboard} content={SprintPage} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/sprintboard' component={Dashboard} content={Sprintboard} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/planning' component={Dashboard} content={Planning} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/planning/game/:id' component={Dashboard} content={PlanningPokerGame} finishLoading={finishLoading} />
+					      <PrivateRoute exact path='/retrospective' component={Dashboard} content={Retrospective} finishLoading={finishLoading} />
+					      <Route render={(props) => (<Error404 finishLoading={finishLoading} {...props} />)} />
+					    </Switch>
+		            </React.Fragment>
+		          )} />
+			    
+		    </Router>
+		    <GlobalStyle />
+	    </div>
 		)
-	}
 }
 
-function mapStateToProps(state) {
-    const { alert } = state;
-    return {
-        alert
-    };
-}
-
-const connectedApp = connect(mapStateToProps)(App);
-const firebaseApp = compose(withFirebase)(connectedApp)
-export { firebaseApp as App }; 
+export { App }; 
