2a732861324d338498fe225313570f23316bd5dd
Andreas <andreasjj@gmail.com>
2019-12-02 09:34:18 +0100

    Added new logo and waves (#77)

--- src/pages/Splash/index.jsx
+++ src/pages/Splash/index.jsx
@@ -2,9 +2,9 @@ import React, {useEffect, useRef} from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import { history } from '../../state/helpers/history';
 
-import LogoImage from '../../assets/Logo.svg'
+import LogoSrc from '../../assets/Logo.png'
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import BackgroundImage from '../../assets/splash/background.gif'
 import welcomeImage from '../../assets/splash/welcome.svg'
@@ -17,16 +17,15 @@ const Content = styled.div`
     height: inherit;
     width: inherit;
     display: grid;
-    grid-template-columns: 100%;
-    grid-template-rows: 50% 50%;
-    background-image: url(${props => props.image});
+    grid-template-columns: 40% 60%;
+    grid-template-rows: Calc(100% - 100px);
+    background-color: #2ECEFE;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover
-
-    @media only screen and (min-width: 768px) {
-      grid-template-columns: 40% 60%;
-      grid-template-rows: 100%;
+    @media only screen and (max-width: 768px) {
+      grid-template-columns: 100%;
+      grid-template-rows: Calc(100% - 100px);
     }
 `
 
@@ -41,12 +40,19 @@ const TextContent = styled.div`
 const LogoWrapper = styled.div`
   display: flex;
   align-content: center;
+  height: auto;
 `
 
 const Logo = styled.div`
-  font-size: 2em;
-  font-weight: bolder;
-  text-align: center;
+  display: block;
+  height: 100%;
+  width: 100%;
+`
+
+const LogoImage = styled.img`
+  display: block;
+  width: 100%;
+  height: auto;
 `
 
 const GetStarted = styled.button`
@@ -62,6 +68,62 @@ const Continue = styled.span`
   font-family: Georgia,Times,'Times New Roman',serif;
 `
 
+const WaveBox = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`
+
+const Waves = styled.svg`
+  position: relative;
+  width: 100%;
+  height: 15vh;
+  margin-bottom: -7px;
+  min-height: 100px;
+  max-height: 150px;
+
+  @media (max-width: 768px) {
+    height: 40px;
+    min-height: 40px;
+  }
+`
+
+const MoveForever = keyframes`
+  0% {
+    transform: translate3d(-90px, 0, 0);
+  }
+  100% {
+    transform: translate3d(85px, 0, 0);
+  }
+`
+
+const Parallax = styled.g`
+  &>use {
+      animation: ${MoveForever} 25s cubic-bezier(.55, .5, .45, .5) infinite;
+  }
+
+  &>use:nth-child(1) {
+    animation-delay: -2s;
+    animation-duration: 7s;
+  }
+
+  &>use:nth-child(2) {
+    animation-delay: -3s;
+    animation-duration: 10s;
+  }
+
+  &>use:nth-child(3) {
+    animation-delay: -4s;
+    animation-duration: 13s;
+  }
+
+  &>use:nth-child(4) {
+    animation-delay: -5s;
+    animation-duration: 20s;
+  }
+`
+
 const LastChance = styled.button`
   padding: 20px;
   width: 200px;
@@ -75,6 +137,10 @@ const SideImage = styled.div`
   justify-content: center;
   align-items: center;
   padding-right: 30px;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 `
 
 const Image = styled.img`
@@ -92,13 +158,10 @@ const HomePage = (props) => {
     if(getCookie("visited")) {
       history.push('/login');
     }
-    props.finishLoading()
   }, [])
 
   useEffect(() => {
-    if(props.location.hash !== prevProps.current.location.hash) {
-      props.finishLoading()
-    }
+    props.finishLoading()
   })
 
   const toRegister = (e) => {
@@ -130,8 +193,7 @@ const HomePage = (props) => {
               <Content image={BackgroundImage}>
                 <TextContent>
                   <LogoWrapper>
-                    <img src={LogoImage} />
-                    <Logo><h1>Agile Toolkit</h1></Logo>
+                    <Logo><LogoImage src={LogoSrc} /></Logo>
                   </LogoWrapper>
                   <h2>Collaborate with your team, write clean code, and make awesome products. Let us take care of scrum!</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet augue tincidunt, efficitur augue eget, elementum dui. Nam lacinia ligula tellus, eget interdum ipsum dictum vel. Nam cursus, elit eget rhoncus vulputate, odio risus gravida mauris, nec maximus nisl purus nec mi. Morbi id pulvinar elit. Vivamus viverra id eros id egestas. Aliquam purus massa, ornare vel viverra eu.</p>
@@ -142,6 +204,19 @@ const HomePage = (props) => {
                   <Image src={welcomeImage} />
                 </SideImage>
               </Content>
+              <WaveBox>
+                <Waves xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <Parallax>
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </Parallax>
+                </Waves>
+              </WaveBox>
             </div>
             <div className="section">
               <Content image={BackgroundImage}>
@@ -153,6 +228,19 @@ const HomePage = (props) => {
                   <Image src={backlogImage} />
                 </SideImage>
               </Content>
+              <WaveBox>
+                <Waves xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <Parallax>
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </Parallax>
+                </Waves>
+              </WaveBox>
             </div>
             <div className="section">
               <Content image={BackgroundImage}>
@@ -164,6 +252,19 @@ const HomePage = (props) => {
                   <Image src={PlanningPokerImage} />
                 </SideImage>
               </Content>
+              <WaveBox>
+                <Waves xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <Parallax>
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </Parallax>
+                </Waves>
+              </WaveBox>
             </div>
             <div className="section">
               <Content image={BackgroundImage}>
@@ -175,6 +276,19 @@ const HomePage = (props) => {
                   <Image src={sprintboardImage} />
                 </SideImage>
               </Content>
+              <WaveBox>
+                <Waves xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <Parallax>
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </Parallax>
+                </Waves>
+              </WaveBox>
             </div>
             <div className="section">
               <Content image={BackgroundImage}>
@@ -187,6 +301,19 @@ const HomePage = (props) => {
                   <Image src={retrospectiveImage} />
                 </SideImage>
               </Content>
+              <WaveBox>
+                <Waves xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <Parallax>
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+                    </Parallax>
+                </Waves>
+              </WaveBox>
             </div>
           </ReactFullpage.Wrapper>
         );
